d5ddd9e41277408d5a0f829486fe0185226aa704
Qudaybergen <orazbaevqudaybergen0@gmail.com>
2024-12-02 00:59:28 +0500

    PhP kursti tawistim

--- blog.php
+++ blog.php
@@ -5,42 +5,27 @@ require 'assets/header.php';
 
 require 'database.php';
 
-$statement = $pdo->prepare("SELECT * FROM qwerty");
+$statement = $pdo->prepare("SELECT * FROM posts");
 $statement->execute();
 
-$qwerty = $statement->fetchAll();
-// var_dump($qwerty);
-// var_dump($qwerty[0]['title']);
 
+$posts = $statement->fetchAll();
+if ($_SERVER['REQUEST_METHOD'] == 'POST' && isset($_POST['DELETE'])) {
+    
+    
+    
+    $post_id = $_POST['post_id'];
+    
+    $statement = $pdo->prepare("DELETE FROM posts WHERE id = ?");
+    $statement->execute([$post_id]);
+    $_SESSION['post-oshirildi'] = 'Post oshti!';
 
-
-
-
-if (isset($_SESSION['post-jaratildi'])) {
-    $message = $_SESSION['post-jaratildi'];
+    header("Location: blog.php");
+    exit; 
 }
 ?>
 
 <body>
-
-
-    <!-- <svg xmlns="http://www.w3.org/2000/svg" class="d-none">
-        <symbol id="check2" viewBox="0 0 16 16">
-            <path d="M13.854 3.646a.5.5 0 0 1 0 .708l-7 7a.5.5 0 0 1-.708 0l-3.5-3.5a.5.5 0 1 1 .708-.708L6.5 10.293l6.646-6.647a.5.5 0 0 1 .708 0z" />
-        </symbol>
-        <symbol id="circle-half" viewBox="0 0 16 16">
-            <path d="M8 15A7 7 0 1 0 8 1v14zm0 1A8 8 0 1 1 8 0a8 8 0 0 1 0 16z" />
-        </symbol>
-        <symbol id="moon-stars-fill" viewBox="0 0 16 16">
-            <path d="M6 .278a.768.768 0 0 1 .08.858 7.208 7.208 0 0 0-.878 3.46c0 4.021 3.278 7.277 7.318 7.277.527 0 1.04-.055 1.533-.16a.787.787 0 0 1 .81.316.733.733 0 0 1-.031.893A8.349 8.349 0 0 1 8.344 16C3.734 16 0 12.286 0 7.71 0 4.266 2.114 1.312 5.124.06A.752.752 0 0 1 6 .278z" />
-            <path d="M10.794 3.148a.217.217 0 0 1 .412 0l.387 1.162c.173.518.579.924 1.097 1.097l1.162.387a.217.217 0 0 1 0 .412l-1.162.387a1.734 1.734 0 0 0-1.097 1.097l-.387 1.162a.217.217 0 0 1-.412 0l-.387-1.162A1.734 1.734 0 0 0 9.31 6.593l-1.162-.387a.217.217 0 0 1 0-.412l1.162-.387a1.734 1.734 0 0 0 1.097-1.097l.387-1.162zM13.863.099a.145.145 0 0 1 .274 0l.258.774c.115.346.386.617.732.732l.774.258a.145.145 0 0 1 0 .274l-.774.258a1.156 1.156 0 0 0-.732.732l-.258.774a.145.145 0 0 1-.274 0l-.258-.774a1.156 1.156 0 0 0-.732-.732l-.774-.258a.145.145 0 0 1 0-.274l.774-.258c.346-.115.617-.386.732-.732L13.863.1z" />
-        </symbol>
-        <symbol id="sun-fill" viewBox="0 0 16 16">
-            <path d="M8 12a4 4 0 1 0 0-8 4 4 0 0 0 0 8zM8 0a.5.5 0 0 1 .5.5v2a.5.5 0 0 1-1 0v-2A.5.5 0 0 1 8 0zm0 13a.5.5 0 0 1 .5.5v2a.5.5 0 0 1-1 0v-2A.5.5 0 0 1 8 13zm8-5a.5.5 0 0 1-.5.5h-2a.5.5 0 0 1 0-1h2a.5.5 0 0 1 .5.5zM3 8a.5.5 0 0 1-.5.5h-2a.5.5 0 0 1 0-1h2A.5.5 0 0 1 3 8zm10.657-5.657a.5.5 0 0 1 0 .707l-1.414 1.415a.5.5 0 1 1-.707-.708l1.414-1.414a.5.5 0 0 1 .707 0zm-9.193 9.193a.5.5 0 0 1 0 .707L3.05 13.657a.5.5 0 0 1-.707-.707l1.414-1.414a.5.5 0 0 1 .707 0zm9.193 2.121a.5.5 0 0 1-.707 0l-1.414-1.414a.5.5 0 0 1 .707-.707l1.414 1.414a.5.5 0 0 1 0 .707zM4.464 4.465a.5.5 0 0 1-.707 0L2.343 3.05a.5.5 0 1 1 .707-.707l1.414 1.414a.5.5 0 0 1 0 .708z" />
-        </symbol>
-    </svg> -->
-
-
     <div class="container py-4">
         <section class="py-5 text-center container">
             <div class="row py-lg-5">
@@ -57,14 +42,22 @@ if (isset($_SESSION['post-jaratildi'])) {
 
         <div class="album py-5 bg-body-tertiary">
             <div class="container">
+                
                 <?php if (isset($_SESSION['post-jaratildi'])): ?>
                     <div class="alert alert-success" role="alert">
                         <?= $_SESSION['post-jaratildi'] ?>
                         <?php unset($_SESSION['post-jaratildi']) ?>
                     </div>
                 <?php endif; ?>
+
+                <?php if (isset($_SESSION['post-oshirildi'])): ?>
+                    <div class="alert alert-danger" role="alert">
+                    <?= $_SESSION['post-oshirildi'] ?>
+                    <?php unset($_SESSION['post-oshirildi']) ?>                    </div>
+                <?php endif; ?>
+
                 <div class="row row-cols-1 row-cols-sm-2 row-cols-md-3 g-3">
-                    <?php foreach ($qwerty as $qwerty) : ?>
+                    <?php foreach ($posts as $post) : ?>
                         <div class="col">
                             <div class="card shadow-sm">
                                 <svg class="bd-placeholder-img card-img-top" width="100%" height="225" xmlns="http://www.w3.org/2000/svg" role="img" aria-label="Placeholder: Thumbnail" preserveAspectRatio="xMidYMid slice" focusable="false">
@@ -73,17 +66,27 @@ if (isset($_SESSION['post-jaratildi'])) {
                                 </svg>
                                 <div class="card-body">
                                     <div>
-                                        <a href="post.php?id=<?= $qwerty['id']?>">
-                                        <h5><?= $qwerty['title']; ?></h5>
+                                        <a href="post.php?id=<?= $post['id'] ?>">
+                                            <h5><?= $post['title']; ?></h5>
                                         </a>
                                     </div>
-                                    <p class="card-text"><?= $qwerty['body']; ?></p>
+                                    <p class="card-text"><?= $post['body']; ?></p>
                                     <div class="d-flex justify-content-between align-items-center">
                                         <div class="btn-group">
-                                            <a href="post.php"><button type="button" class="btn btn-sm btn-outline-secondary">View</button></a>
-                                            <a href="post.php"> <button type="button" class="btn btn-sm btn-outline-secondary">Edit</button></a>
+                                            <a href="post-edit.php?id=<?= $post['id'];?>"> <button class="btn btn-sm btn-outline-secondary">Edit</button></a>
+
+                                            <form method="POST" action="" onsubmit="return confirm('Aniq oshirejaqsizba ?')">
+
+                                                <input type="hidden" name="post_id" value="<?= $post['id']; ?>">
+
+                                                <input type="hidden" name="DELETE">
+
+                                                <button type="submit" class="btn btn-sm btn-outline-secondary">Delete</button>
+
+                                            </form>
+                                            
                                         </div>
-                                        <small class="text-body-secondary"><?= $qwerty['created_at'] ?></small>
+                                        <small class="text-body-secondary"><?= $post['created_at'] ?></small>
                                     </div>
                                 </div>
                             </div>
